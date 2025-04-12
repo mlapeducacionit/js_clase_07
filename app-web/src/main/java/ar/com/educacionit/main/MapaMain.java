ff51443d42417f7823e4108ce63106e5e5d00afc
Maximiliano Principe <mleducacion@gmail.com>
2025-04-12 13:00:34 -0300

    Agregamos la creación y carga de un mapa

--- app-web/src/main/java/ar/com/educacionit/main/MapaMain.java
+++ app-web/src/main/java/ar/com/educacionit/main/MapaMain.java
@@ -1,10 +1,24 @@
 package ar.com.educacionit.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapaMain {
 
 	public static void main(String[] args) {
 		System.out.println("Mapa");
-
+		// Crea
+		Map<Integer, String> diasSemana = new HashMap<>();
+		
+		//carga
+		diasSemana.put(1, "Lunes");
+		diasSemana.put(2, "Martes");
+		diasSemana.put(3, "Miercoles");
+		diasSemana.put(4, "Jueves");
+		diasSemana.put(5, "Viernes");
+		diasSemana.put(6, "Sabado");
+		diasSemana.put(7, "Domingo");
+		System.out.println(diasSemana);
 	}
 
 }
